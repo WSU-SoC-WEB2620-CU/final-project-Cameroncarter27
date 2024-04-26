e29ba681e4332e481cb567649ff699ec4b2747de
Cameron Carter <cameroncarter1@mail.weber.edu>
2024-04-25 23:31:50 -0600

    Final Submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,3 +4,25 @@ require.context('../images/', true, /\.(png|jpg|jpeg|gif|svg)$/i)
 require("../stylesheets/main.scss")
 
 //TODO - Your ES6 JavaScript code (if any) goes here
+import * as fontawesome from "@fortawesome/fontawesome-free/js/all"
+import "bootstrap"
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
